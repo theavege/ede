b76680d03d89eb95a1aeebee9741c04f00b03a79
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-10-16 17:04:28 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ function main
     if [[ ${RUNNER_OS} == "Linux" ]]; then
         git submodule update --init --recursive
         sudo apt-get update
+        sudo apt-get search python2
         sudo apt-get install -y python2.7 curl xorg fltk1.3-dev jam libdbus-1-dev
         for item in "edelib" "."; do
             pushd "${item}" || return 1
